ee2be694476c8ee152bbb9647c80737ca8f4dcbc
gaogaostar <115141870+gaogaostar@users.noreply.github.com>
2022-11-21 17:48:54 +0000

    [Add]各ページのリンク

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,45 @@
+class BooksController < ApplicationController
+
+  def index
+    @newbook = Book.new
+    @books = Book.all
+    @user = current_user
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to book_path(@book.id)
+  end
+
+  def show
+    @newbook = Book.new
+    @books = Book.all
+    @book = Book.find(params[:id])
+    @user = current_user
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.delete
+    redirect_to books_path
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
+end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,39 @@
+class UsersController < ApplicationController
+  before_action :is_matching_login_user, only: [:edit, :update]
+
+  def index
+    @newbook = Book.new
+    @user = current_user
+    @users = User.all
+  end
+
+  def show
+    @newbook = Book.new
+    @user = User.find(params[:id])
+    @books = @user.books
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user.id)
+  end
+
+private
+
+  def user_params
+    params.require(:user).permit(:name, :profile_image, :introduction)
+  end
+
+  def is_matching_login_user
+    user_id = params[:id].to_i
+    login_user_id = current_user.id
+    if(user_id != login_user_id)
+      redirect_to user_path(login_user_id)
+    end
+  end
+end
